657021f5a18b40f02a9aaa6c7de088b64a615880
Kazumasa Sasaya <sasaya.kazumasa@ichikara.co.jp>
2018-09-20 20:09:05 +0900

    FitstCommit

---
+++ Assets/Scripts/GetYoutubeAPI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Ichikara.GetYoutubeComment
+{
+    public class GetYoutubeAPI : MonoBehaviour
+    {
+        private static string URI = "";
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        private void test()
+        {
+
+        }
+    }
+}
